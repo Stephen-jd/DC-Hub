7c42212444dec69e595a94fe04851db41f5c3b64
Stephen-jd <stephenjdurai@gmail.com>
2026-05-26 13:42:59 +0530

    Update ALLOWED_HOSTS configuration to allow wildcard domains for Vercel

--- dc_master/settings.py
+++ dc_master/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--vw)rh*5gpn&su952&kcugzg1-rsi!@)e-vtp(fi@4os_f4qh9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
